3b9b5fb8c14dec8286fc227def00f0eae03fd774
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-12-31 16:24:28 -0500

    basic example updated

--- basic_app.py
+++ basic_app.py
@@ -2,13 +2,12 @@ import os
 from selenium import webdriver 
 from selenium.webdriver.chrome.service import Service
 
+# Hook up selenium to driver
 s = Service('/user/local/bin/chromerdriver')
 driver = webdriver.Chrome()
 
+# Point driver towards desired URL and take screenshot
 driver.get("http://www.python.org")
-
 driver.implicitly_wait(1)
-
 driver.save_screenshot("Screeshot.png")
-
 driver.quit()
